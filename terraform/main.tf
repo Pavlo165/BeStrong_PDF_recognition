7c53698b6115d8eee66a0d70e83d54fa10e97d24
Mochurad Pavlo <mothuradpavlo@gmail.com>
2025-01-01 19:29:44 +0200

    fix prod pipe

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,7 +54,7 @@ resource "azurerm_linux_function_app" "function_app" {
   }
 
   app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME" = "python" # тест
+    "FUNCTIONS_WORKER_RUNTIME" = "python" # тест 
     "WEBSITE_RUN_FROM_PACKAGE" = "1"     # Налаштування для запуску із пакету 
   }
 
