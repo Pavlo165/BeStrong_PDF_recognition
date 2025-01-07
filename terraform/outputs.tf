9d5ede7d72bc55bb3d6e1dea8ab75241e8bcd78b
Mochurad Pavlo <mothuradpavlo@gmail.com>
2025-01-07 12:44:40 +0200

    fix output terrafrom

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,13 +11,13 @@ output "storage_account_primary_key" {
 }
 
 # Output для Storage Container archive
-output "blob_container_name" {
+output "blob_container_name_achive" {
   description = "The name of the created Blob Container"
   value       = azurerm_storage_container.blob_container_archive.name
 }
 
 # Output для Storage Container import
-output "blob_container_name" {
+output "blob_container_name_import" {
   description = "The name of the created Blob Container"
   value       = azurerm_storage_container.blob_container_import.name
 }
